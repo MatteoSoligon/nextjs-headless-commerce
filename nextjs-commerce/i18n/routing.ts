250cfff64daa67ef2a69b53a8b00b8016007a8c0
Matteo Soligon <matteo.soligon@arsenalia.com>
2026-01-31 15:23:04 +0100

    set up tailwind theming

--- nextjs-commerce/i18n/routing.ts
+++ nextjs-commerce/i18n/routing.ts
@@ -1,8 +1,9 @@
 import {defineRouting} from 'next-intl/routing';
+import {locales} from '../config/localeConfig';
  
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ['en', 'de'],
+  locales: locales,
  
   // Used when no locale matches
   defaultLocale: 'en',
